f38a4f54da1d7c5531c05ad5107e7c765051eec5
TBlais10 <blaislighting@gmail.com>
2022-04-21 21:23:53 -0400

    Initial commit

---
+++ src/main/java/com/example/BigDataBitcoin/BigDataBitcoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.BigDataBitcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataBitcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDataBitcoinApplication.class, args);
+	}
+
+}
