745677e2df6207ac5a8df54b157c095f01931002
TBlais10 <blaislighting@gmail.com>
2022-04-23 00:20:19 -0400

    Updating backend and bringing a lot of dinos back from the grae.

---
+++ src/main/java/Repositories/RoleRepository.java
@@ -0,0 +1,22 @@
+package Repositories;
+
+import Auth.ERole;
+import Auth.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+
+    @Query(value = "select count(*) from role",
+            nativeQuery = true)
+    int isRoleEmpty();
+
+    @Query(value = "Insert into role (name) name values(:roleName) ",
+            nativeQuery = true)
+    Set<ERole> insertRoles(@Param("roleName") ERole role);
+}
--- src/main/java/com/example/BigDataBitcoin/BigDataBitcoinApplication.java
+++ src/main/java/com/example/BigDataBitcoin/BigDataBitcoinApplication.java
@@ -1,13 +1,67 @@
 package com.example.BigDataBitcoin;
 
+import Auth.ERole;
+import Repositories.RoleRepository;
+import Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 @SpringBootApplication
 public class BigDataBitcoinApplication {
+	@Autowired
+	private UserRepository repository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Value("${spring.datasource.driver-class-name}")
+	public String myDriver;
+
+	@Value("${spring.datasource.url}")
+	public String myUrl;
+
+	@Value("${spring.datasource.username}")
+	public String username;
+
+	@Value("${spring.datasource.password}")
+	public String password;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BigDataBitcoinApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		int roleCheck = roleRepository.isRoleEmpty();
+
+		if (roleCheck < ERole.values().length){
+			int id = 1;
+			for (ERole role : ERole.values()){
+				try	{
+					Connection conn = DriverManager.getConnection(myUrl, username, password);
+					Class.forName(myDriver);
+					String query = "Insert into role (id, name) values (/,/";
+					PreparedStatement statement = conn.prepareStatement(query);
+
+					statement.setString(1, Integer.toString(id));
+					statement.setString(2, role.toString());
+
+					statement.executeUpdate();
+				} catch (Exception e){
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+		return builder.build();
 	}
 
 }
